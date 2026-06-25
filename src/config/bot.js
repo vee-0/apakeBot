245f8ed646403c87ed6837225bbfaae51fa13041
vee-0 <cabreltchaptchet81@gmail.com>
2026-06-25 03:21:08 -0400

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,7 +24,7 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Je suis APAKE bot",
         // Activity type number (0 = Playing).
         type: 0,
       },
@@ -91,7 +91,7 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
+      primary: "#000000",
       secondary: "#2F3136",
 
       // Standard status colors for success/error/warning/info messages.
